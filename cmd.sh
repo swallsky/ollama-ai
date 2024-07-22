a0f6ec3cf3c819343a79d797d4b9a6cba8f43c76
jackXu <xujinzhang@372163.com>
2024-07-22 17:32:35 +0800

    fixed bug

--- cmd.sh
+++ cmd.sh
@@ -32,14 +32,16 @@ ollama_list(){
   docker compose exec ollama /usr/bin/ollama list
 }
 
-# 下载gemma2(9B)大模型
+# 下载并运行gemma2(9B)大模型
 gemma2_9B(){
-  docker compose exec ollama /usr/bin/ollama pull gemma2
+  # docker compose exec ollama /usr/bin/ollama pull gemma2
+  docker compose exec ollama /usr/bin/ollama run gemma2
 }
 
-# 下载gemma2(27B)大模型
+# 下载并运行gemma2(27B)大模型
 gemma2_27B(){
-  docker compose exec ollama /usr/bin/ollama pull gemma2:27b
+  # docker compose exec ollama /usr/bin/ollama pull gemma2:27b
+  docker compose exec ollama /usr/bin/ollama run gemma2:27b
 }
 
 # 使用说明
@@ -51,8 +53,8 @@ cat << EOF
     启动基础服务 start
     查看容器状态 ps
     显示大模型列表 ollama_list
-    下载gemma2(9B)大模型 gemma2_9B
-    下载gemma2(27B)大模型 gemma2_27B
+    下载并运行gemma2(9B)大模型 gemma2_9B
+    下载并运行gemma2(27B)大模型 gemma2_27B
     查看日志 logs
     关闭容器 down
 EOF
